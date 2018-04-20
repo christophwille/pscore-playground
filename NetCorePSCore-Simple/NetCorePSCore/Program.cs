a575791f7e44d24b14a5f893af879f73699994ff
Christoph Wille <christoph.wille@gmail.com>
2018-04-20 14:51:35 +0200

    Minimal sample

---
+++ NetCorePSCore-Simple/NetCorePSCore/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace NetCorePSCore
+{
+    class Program
+    {
+        // "When will the NuGet package be on nuget.org?" https://github.com/PowerShell/PowerShell/issues/6475
+
+        // Official documentation https://github.com/PowerShell/PowerShell/tree/master/docs/host-powershell
+        // SO https://stackoverflow.com/questions/39141914/running-powershell-from-net-core
+        // 
+        static void Main(string[] args)
+        {
+            using (var ps = PowerShell.Create())
+            {
+                var results = ps.AddScript("$PSVersionTable | Out-String").Invoke();
+
+                foreach (var result in results)
+                {
+                    Console.Write(result.ToString());
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
