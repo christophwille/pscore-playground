b6e6914da6eb2333e697c5b85f2dab8254c20253
Christoph Wille <christoph.wille@gmail.com>
2018-04-23 10:28:11 +0200

    WSMan version of code (most likely a Windows-only solution atm)

--- NetCorePSCore-Simple/NetCorePSCore/Program.cs
+++ NetCorePSCore-Simple/NetCorePSCore/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Security;
 
 namespace NetCorePSCore
 {
@@ -23,6 +25,7 @@ namespace NetCorePSCore
 			string pass = Console.ReadLine();
 
 			SimpleExO(admin, pass);
+			// WSManConnectToExO(admin, pass);
 
 
 			Console.Read();
@@ -82,6 +85,53 @@ Get-Mailbox | Out-String".Replace("REPLACEUSER", admin).Replace("REPLACEPASS", p
 				{
 					Console.Write(result.ToString());
 				}
+			}
+		}
+
+		static void WSManConnectToExO(string admin, string pass)
+		{
+			using (var ps = PowerShell.Create())
+			{
+				var secureString = new SecureString();
+				pass.ToCharArray().ToList().ForEach(p => secureString.AppendChar(p));
+				var credential = new PSCredential(admin, secureString);
+
+				var connectionInfo = new WSManConnectionInfo(
+					new Uri("https://outlook.office365.com/powershell-liveid/"),
+					"http://schemas.microsoft.com/powershell/Microsoft.Exchange",
+					credential);
+				connectionInfo.AuthenticationMechanism = AuthenticationMechanism.Basic;
+				connectionInfo.MaximumConnectionRedirectionCount = 2;
+
+				var pool = RunspaceFactory.CreateRunspacePool(1, 5, connectionInfo);
+				pool.ThreadOptions = PSThreadOptions.UseNewThread;
+				pool.Open();
+
+				ps.RunspacePool = pool;
+
+				ps.AddCommand("Get-Mailbox");
+				ps.AddParameter("Identity", admin);
+
+				ps.AddCommand("Select-Object");
+				ps.AddParameter("ExpandProperty", "EmailAddresses");
+				List<PSObject> results = ps.Invoke().ToList();
+
+				if (ps.Streams.Error.Count > 0)
+				{
+					Console.WriteLine("!Errors! " + String.Join(" :: ", ps.StreamErrorsToErrorList()));
+				}
+
+				if (!results.Any())
+					Console.WriteLine("Identity not found");
+
+				var addressesArray = results.Select(psObject => psObject.Properties["ProxyAddressString"].Value.ToString()).ToList();
+				var aliasAddresses = (from address in addressesArray where address.StartsWith("smtp:", StringComparison.InvariantCulture) select address.Substring("smtp:".Length)).ToList();
+				var addresses = addressesArray.Where(a => !a.StartsWith("SMTP:")).ToList();
+
+				Console.WriteLine("Alias addresses: " + String.Join(",", aliasAddresses));
+				Console.WriteLine("SMTP: " + String.Join(",", addresses));
+
+				pool.Close();
 			}
 		}
 	}
