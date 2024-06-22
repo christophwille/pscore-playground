0b65519cc419b054cea054934abc6f6bf9307c57
Christoph Wille <christoph.wille@gmail.com>
2024-06-22 15:06:52 +0200

    Go back to iss.ThrowOnRunspaceOpenError = true;

--- ExO3onNet8LocalModule/ExO3PsLib/ExchangeOnlinePowerShellFactory.cs
+++ ExO3onNet8LocalModule/ExO3PsLib/ExchangeOnlinePowerShellFactory.cs
@@ -29,7 +29,7 @@ namespace ExO3PsLib
                 iss.ExecutionPolicy = Microsoft.PowerShell.ExecutionPolicy.Unrestricted;
             }
 
-            iss.ThrowOnRunspaceOpenError = false;
+            iss.ThrowOnRunspaceOpenError = true;
             iss.ImportPSModule(new string[] { modulePath }); // would be "ExchangeOnlineManagement" for installed PS module
 
             return iss;
